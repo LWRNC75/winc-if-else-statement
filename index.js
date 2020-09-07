e6168ab1f09188fecbb7eb3df2fc31a34a7a688a
LWRNC75 <lawrence.kartodimedjo@gmail.com>
2020-09-07 23:00:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+  console.log("You May Enter");
+} else {
+  console.log("Sorry, You Are Not Allowed");
+}
+
+if (isFemale) {
+  console.log("You are a women according to your ticket");
+} else {
+  console.log("Sorry, you are a man, You are not allowed!");
+}
+
+if (driverStatus === "bob") {
+  console.log("Hi Bob, you can Drive!");
+} else {
+  console.log("You are not Bob, give me the keys!");
+}
